4bbcfda1a3472dc87ed0dd0cd04ccf3c5510cdb8
RomyMartinez <123706429+RomyMartinez@users.noreply.github.com>
2025-07-28 20:04:57 -0300

    initial commit

---
+++ src/test/java/com/romy/clinica/clinica/ClinicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.romy.clinica.clinica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
