b4438ab3233c79d32533e2e7fa93a9b6c7eb0b2f
RomyMartinez <123706429+RomyMartinez@users.noreply.github.com>
2025-08-03 23:25:08 -0300

    feat: Implementing useUser

---
+++ frontend/clinica_odontologica/src/hooks/useUser.ts
@@ -0,0 +1,21 @@
+import { useQuery } from "@tanstack/react-query";
+import api from "../services/api";
+
+export function useUser() {
+  return useQuery({
+    queryKey: ["user"],
+    queryFn: getUser,
+  });
+}
+
+export async function getUser() {
+  try {
+    const response = await api.get("users/get");
+    return response.data;
+  } catch (err: any) {
+    if (err.response?.status === 401 || err.response?.status === 403) {
+    }
+
+    throw new Error("Error " + err.message);
+  }
+}
