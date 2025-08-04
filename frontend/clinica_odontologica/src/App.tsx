27453accd06a0aad72fdd8be4f0cf2a328f83a55
RomyMartinez <123706429+RomyMartinez@users.noreply.github.com>
2025-08-03 23:27:16 -0300

    feat: Implemnting routes

--- frontend/clinica_odontologica/src/App.tsx
+++ frontend/clinica_odontologica/src/App.tsx
@@ -1,38 +1,52 @@
 import "./global.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { PrivateRoute } from "./routes/PrivateRoutes";
 import { Login } from "./pages/Login";
 import { Dashboard } from "./pages/Dashboard";
 import { Pacientes } from "./pages/Pacientes";
 import { Consultas } from "./pages/Consultas";
 import { Settings } from "./pages/Settings";
 import { AuthProvider } from "./contexts/AuthContext";
+import { PrivateRoute } from "./routes/PrivateRoutes";
+import { Sidebar } from "./components/sidebar/Sidebar";
+import { ErrorPage } from "./pages/ErrorPage";
 
 function App() {
   const queryClient = new QueryClient();
+  const router = createBrowserRouter([
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/",
+      element: <PrivateRoute page={<Sidebar />} />,
+      errorElement: <ErrorPage />,
+      children: [
+        {
+          index: true,
+          element: <Dashboard />,
+        },
+        {
+          path: "pacientes",
+          element: <Pacientes />,
+        },
+        {
+          path: "consultas",
+          element: <Consultas />,
+        },
+        {
+          path: "settings",
+          element: <Settings />,
+        },
+      ],
+    },
+  ]);
 
   return (
     <AuthProvider>
       <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path="/" element={<PrivateRoute page={<Dashboard />} />} />
-            <Route
-              path="/pacientes"
-              element={<PrivateRoute page={<Pacientes />} />}
-            />
-            <Route
-              path="/consultas"
-              element={<PrivateRoute page={<Consultas />} />}
-            />
-            <Route
-              path="/settings"
-              element={<PrivateRoute page={<Settings />} />}
-            />
-          </Routes>
-        </BrowserRouter>
+        <RouterProvider router={router} />
       </QueryClientProvider>
     </AuthProvider>
   );
