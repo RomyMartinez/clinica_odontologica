4133a02c6a34b100493b16197c1c22bac2b40dba
RomyMartinez <123706429+RomyMartinez@users.noreply.github.com>
2025-08-04 23:19:52 -0300

    feat: Implemeting Consulta interface

---
+++ frontend/clinica_odontologica/src/components/interfaces/consulta.ts
@@ -0,0 +1,14 @@
+import type { Paciente } from "./paciente";
+import type { Dentista } from "./dentista";
+
+export interface Consulta {
+  id: string;
+  dataHora: string;
+  descricao: string;
+  status: "AGENDADA" | "CANCELADA" | "CONCLUIDA" | "REAGENDADA";
+  patientId: string;
+  dentistId: string;
+  paciente: Paciente;
+  dentista: Dentista;
+  createdAt: string;
+}
