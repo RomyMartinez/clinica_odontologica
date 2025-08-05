6b0c8cb74b7b18216571c651464b812f2046c5eb
RomyMartinez <123706429+RomyMartinez@users.noreply.github.com>
2025-08-04 23:22:41 -0300

    feat: Implementing hook usePacientes

---
+++ frontend/clinica_odontologica/src/hooks/usePacientes.ts
@@ -0,0 +1,31 @@
+import { useQuery } from "@tanstack/react-query";
+import api from "../services/api";
+import type { Paciente as pacienteInterface } from "../components/interfaces/paciente";
+
+export function usePacients() {
+  return useQuery({
+    queryKey: ["pacientes"],
+    queryFn: getPacientes,
+  });
+}
+
+export async function getPacientes(): Promise<pacienteInterface[]> {
+  try {
+    const response = await api.get<pacienteInterface[]>("/paciente/findAll");
+    return response.data;
+  } catch (error: any) {
+    const status = error?.response?.status;
+
+    if (status === 401) {
+      throw new Error("Não autorizado. Faça login novamente.");
+    } else if (status === 403) {
+      throw new Error("Acesso negado.");
+    } else if (status === 404) {
+      throw new Error("Pacientes não encontrados.");
+    } else if (status >= 500) {
+      throw new Error("Erro no servidor. Tente mais tarde.");
+    }
+
+    throw new Error("Erro ao buscar pacientes: " + error.message);
+  }
+}
