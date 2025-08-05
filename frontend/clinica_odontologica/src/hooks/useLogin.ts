dcbf8cbb49b289dc98f157048d2ac3afc7be18b1
RomyMartinez <123706429+RomyMartinez@users.noreply.github.com>
2025-08-04 23:22:25 -0300

    feat: Implementing hook useLogin

--- frontend/clinica_odontologica/src/hooks/useLogin.ts
+++ frontend/clinica_odontologica/src/hooks/useLogin.ts
@@ -1,8 +1,13 @@
 import { useMutation } from "@tanstack/react-query";
-import { loginUser } from "../services/auth";
+import api from "../services/api";
 
 export function useLogin() {
   return useMutation({
     mutationFn: loginUser,
   });
+}
+
+export async function loginUser(data: { username: string; password: string }) {
+  const response = await api.post("/auth/login", data);
+  return response.data;
 }
