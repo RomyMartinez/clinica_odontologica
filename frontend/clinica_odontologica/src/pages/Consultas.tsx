ff4aada6428c4694031454dd06d5db5ec95a4918
RomyMartinez <123706429+RomyMartinez@users.noreply.github.com>
2025-08-08 01:19:12 -0300

    feat: integrar componentes de consulta na página Consultas
    - Integra GridConsulta na página de consultas
    - Adiciona hook useConsultas para buscar dados
    - Implementa estado de loading
    - Melhora layout com flexbox para melhor responsividade
    - Conecta interface com dados reais das consultas

--- frontend/clinica_odontologica/src/pages/Consultas.tsx
+++ frontend/clinica_odontologica/src/pages/Consultas.tsx
@@ -1,15 +1,29 @@
 import { Header } from "../components/ui/Header";
 import { AddBar } from "../components/ui/AddBar";
+import { GridConsulta } from "../components/Consulta.tsx/GridConsulta";
+import { useConsultas } from "../hooks/useConsultas";
 
 export function Consultas() {
+  const { data: consultasData, isLoading } = useConsultas();
+
+  const consultas = consultasData || [];
+  if (isLoading) {
+    return <div>Carregando...</div>;
+  }
+
   return (
-    <div>
+    <div className="h-full flex flex-col">
       <Header title="Consultas" />
-      <AddBar
-        title="Maneje suas consultas"
-        buttonLabel="Nova Consulta"
-        onClick={() => {}}
-      />
+      <div className="flex-1 flex flex-col min-h-0">
+        <AddBar
+          title="Maneje suas consultas"
+          buttonLabel="Nova Consulta"
+          onClick={() => {}}
+        />
+        <div className="flex-1 min-h-0">
+          <GridConsulta consultas={consultas} />
+        </div>
+      </div>
     </div>
   );
 }
